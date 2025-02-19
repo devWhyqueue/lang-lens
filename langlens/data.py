88fc93105ea07fec0438d60b641d65421a4898d6
Yannik Queisler <dev.yannik.queisler@gmail.com>
2025-02-19 12:52:06 +0100

    Add some dataset insights

--- langlens/data.py
+++ langlens/data.py
@@ -28,6 +28,8 @@ def load_and_split(dataset_path: str) -> Tuple[Dataset, Dataset, Dataset]:
         Tuple[Dataset, Dataset, Dataset]: A tuple containing the training, validation, and test datasets.
     """
     df = pd.read_csv(dataset_path)
+    df = _clean_data(df)
+
     x, y = df['text'], df['language']
     x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.2, random_state=settings['seed'])
     x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=settings['seed'])
@@ -37,3 +39,29 @@ def load_and_split(dataset_path: str) -> Tuple[Dataset, Dataset, Dataset]:
         Dataset(x_val.values, y_val.values),
         Dataset(x_test.values, y_test.values)
     )
+
+
+def _clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean the dataset by performing the following operations:
+    1. Remove rows where the 'text' column contains only whitespace.
+    2. Correct the typo in the 'language' column from 'Portugese' to 'Portuguese'.
+    3. Map language names to their corresponding ISO 639-3 codes using a mapping file.
+
+    Args:
+        data (pd.DataFrame): The input dataset containing 'text' and 'language' columns.
+
+    Returns:
+        pd.DataFrame: The cleaned dataset with an additional 'lang_code' column.
+    """
+    data = data.copy()
+    # Clean the dataset by removing whitespace-only texts
+    data = data[data['text'].str.strip() != ""]
+    # Correct the typo in 'Portuguese'
+    data['language'] = data['language'].replace('Portugese', 'Portuguese')
+    # Map language names to ISO 639-3 codes
+    mapping_df = pd.read_csv("../data/lang_codes.csv")
+    iso_639_3_codes = dict(zip(mapping_df['language'], mapping_df['lang_code']))
+    data['lang_code'] = data['language'].map(iso_639_3_codes)
+
+    return data
