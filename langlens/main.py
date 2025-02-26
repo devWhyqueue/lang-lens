81e4aa59f8ee163bc846b5e72f1a9cc2c92fe3aa
Yannik Queisler <dev.yannik.queisler@gmail.com>
2025-02-26 12:02:43 +0100

    Add val-for-training option (#24)

--- langlens/main.py
+++ langlens/main.py
@@ -1,12 +1,13 @@
 import click
+import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import normalize
 
 from langlens.baseline.vectorize import vectorize_data
 from langlens.configuration.config import get_logger
-from langlens.data import load, split
+from langlens.data import load, split, Dataset
 from langlens.data import preprocess as preprocess_data
-from langlens.evaluation import evaluate_model
+from langlens.evaluation import evaluate_model_performance
 
 log = get_logger(__name__)
 
@@ -20,15 +21,17 @@ def cli():
 @cli.command(help="Run the baseline model on the provided dataset.")
 @click.option('--dataset-path', required=True, type=click.Path(exists=True), help="Path to the dataset CSV file.")
 @click.option("--preprocess", is_flag=True, help="Flag to indicate whether to preprocess the text data.")
+@click.option("--val-for-training", is_flag=True, help="Flag to include the validation set for training.")
 @click.option('--n-gram-type', type=click.Choice(['word', 'char']), help="Type of n-gram to use for vectorization.")
 @click.option('--vocab-size', type=int, help="Size of the vocabulary to use for vectorization.")
-def baseline(dataset_path: str, preprocess: bool, n_gram_type: str, vocab_size: int):
+def baseline(dataset_path: str, preprocess: bool, val_for_training: bool, n_gram_type: str, vocab_size: int):
     """
     Run the baseline model on the provided dataset.
 
     Args:
         dataset_path (str): Path to the dataset CSV file.
         preprocess (bool): Flag to indicate whether to preprocess the text data.
+        val_for_training (bool): Flag to include the validation set for training.
         n_gram_type (str): Type of n-gram to use for vectorization.
         vocab_size (int): Size of the vocabulary to use for vectorization.
     """
@@ -40,6 +43,10 @@ def baseline(dataset_path: str, preprocess: bool, n_gram_type: str, vocab_size: 
         data_frame = preprocess_data(data_frame)
 
     train_data, val_data, test_data = split(data_frame)
+    if val_for_training:
+        log.info("Including validation set for training.")
+        train_data = Dataset(np.concatenate([train_data.x, val_data.x]), np.concatenate([train_data.y, val_data.y]))
+        val_data = test_data
     log.info("Training dataset size: %d, Val/Test dataset size: %d", len(train_data), len(val_data))
 
     log.info("Vectorizing and normalizing data...")
@@ -50,16 +57,7 @@ def baseline(dataset_path: str, preprocess: bool, n_gram_type: str, vocab_size: 
     model = MultinomialNB()
     model.fit(train_data.x, train_data.y)
 
-    language_set = set(train_data.y) | set(val_data.y) | set(test_data.y)
-    log.info("Training set performance:")
-    y_train_pred = model.predict(train_data.x)
-    evaluate_model(y_train_pred, train_data, train_data, language_set)
-    log.info("Validation set performance:")
-    y_val_pred = model.predict(val_data.x)
-    evaluate_model(y_val_pred, train_data, val_data, language_set)
-    log.info("Test set performance:")
-    y_test_pred = model.predict(test_data.x)
-    evaluate_model(y_test_pred, train_data, test_data, language_set)
+    evaluate_model_performance(model, train_data, val_data, test_data)
 
 
 if __name__ == "__main__":
